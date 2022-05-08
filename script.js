84c35214205e601f7b912d69dec165d75a5bae65
Dima Tarasov <dmytrotarasov9@gmail.com>
2022-05-08 11:46:49 +0200

    solution for second Closure task

---
+++ script.js
@@ -0,0 +1,71 @@
+const app = () => {
+	const inputUrl = document.querySelector("#urlInput");
+	const btn = document.querySelector(".btn");
+	const textArea = document.querySelector(".card-text");
+
+	const request = makeRequestUsingURL();
+	const usedUrls = [];
+	const urlFromUser = {
+		url: "http://localhost:3000/employees",
+		errorMessage: "Sorry, this url has already been",
+	};
+	inputUrl.value = urlFromUser.url;
+
+	const checkUrl = (url) => {
+		if (usedUrls.includes(url)) {
+			textArea.textContent = urlFromUser.errorMessage;
+			return "this url has already been";
+		}
+		usedUrls.push(url);
+		return url;
+	};
+
+	const getValueFromInput = (e) => {
+		urlFromUser.url = e.target.value;
+	};
+
+	const makeRequest = (e) => {
+		e.preventDefault();
+		request(checkUrl(urlFromUser.url)).then((data) => {
+			textArea.appendChild(createContent(data));
+		});
+	};
+	const createContent = (data) => {
+		const content = document.createElement("div");
+
+		if (data.length > 0) {
+			const listOfItems = document.createElement("ul");
+			data.forEach((item) => {
+				const itemOfList = document.createElement("li");
+				itemOfList.innerHTML = `
+				Name: ${item.first_name},
+				Last name: ${item.last_name},
+				Email: ${item.email}
+				`;
+				listOfItems.appendChild(itemOfList);
+			});
+			content.appendChild(listOfItems);
+			return content;
+		}
+		content.textContent = `
+			Name: ${data.first_name},
+			Last name: ${data.last_name},
+			Email: ${data.email}
+		`;
+
+		return content;
+	};
+
+	const init = () => {
+		inputUrl.addEventListener("change", (e) => getValueFromInput(e));
+		btn.addEventListener("click", (e) => makeRequest(e));
+	};
+	return {
+		init: init,
+		destroy: () => {
+			inputUrl.removeEventListener("change", getValueFromInput);
+			btn.removeEventListener("click", makeRequest);
+		},
+	};
+};
+app().init();
---
+++ urlRequest.js
@@ -0,0 +1,35 @@
+const makeRequestUsingURL = () => {
+	const request = async (url) => {
+		try {
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error(
+					`Could not fetch ${url}, status: ${response.status}`
+				);
+			}
+			const data = await response.json();
+			return data;
+		} catch (error) {
+			throw error;
+		}
+	};
+	return request;
+};
+
+
+// const people = [
+// 	{ name: "Alice", age: 21 },
+// 	{ name: "Max", age: 20 },
+// 	{ name: "Jane", age: 20 },
+// ];
+
+// const groupBy = (objectArray, property) => {
+// 	return group = objectArray.reduce((result, person) => {
+//         if(!result[person.age]) {
+//             result[person.age]= [];
+//         }
+//         result[person.age].push(person)
+//         return result
+//     }, {});
+// };
+// console.log(groupBy(people, "age"));
