ba344cdd0c6e357e7207d350485debd9bbf39bb0
Dima Tarasov <dmytrotarasov9@gmail.com>
2022-05-08 19:28:32 +0200

    added loaderto previous task

--- script.js
+++ script.js
@@ -2,36 +2,56 @@ const app = () => {
 	const inputUrl = document.querySelector("#urlInput");
 	const btn = document.querySelector(".btn");
 	const textArea = document.querySelector(".card-text");
+	const spinner = document.querySelector(".loading");
 
 	const request = makeRequestUsingURL();
 	const usedUrls = [];
-	
 
 	const urlFromUser = {
-		url: "http://localhost:3000/employees",
-		errorMessage: "Sorry, this url has already been",
+		url: "https://jsonplaceholder.typicode.com/todos/1",
+		errorMessage: "Sorry, this url has already been, try another url",
 	};
 	inputUrl.value = urlFromUser.url;
 
 	const checkUrl = (url) => {
 		if (usedUrls.includes(url)) {
-			textArea.textContent = urlFromUser.errorMessage;
 			return "this url has already been";
 		}
+		
 		usedUrls.push(url);
 		return url;
 	};
 
 	const getValueFromInput = (e) => {
 		urlFromUser.url = e.target.value;
+		btn.disabled = false;
 	};
-
 	const makeRequest = (e) => {
 		e.preventDefault();
-		request(checkUrl(urlFromUser.url)).then((data) => {
-			textArea.appendChild(createContent(data));
-		});
+
+		loading(true);
+		setTimeout(() => {
+			request(checkUrl(urlFromUser.url))
+				.then((data) => {
+					textArea.appendChild(createContent(data));
+				})
+				.then(loading(false))
+				.catch(() => {
+					errorMessage();
+				});
+		}, 1000);
 	};
+	const loading = (load) => {
+		if (load) {
+			textArea.textContent = "";
+		}
+		spinner.classList.toggle("active");
+		btn.disabled = load;
+	};
+	const errorMessage = () => {
+		btn.disabled = true;
+		textArea.textContent = urlFromUser.errorMessage;
+	}
 	const createContent = (data) => {
 		const content = document.createElement("div");
 
